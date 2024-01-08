90b167c0bf1389cac9d129f8464a2bbda733c664
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-01-08 23:01:10 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (22.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int l=0;
-         String word = s.trim();
-        for(int i=word.length()-1; i>0; i--){
-            if(word.charAt(i)==' '){
-                return word.length()-i-1;
+         s = s.trim();
+        for(int i=s.length()-1; i>0; i--){
+            if(s.charAt(i)==' '){
+                return s.length()-i-1;
             }
         }
-        return word.length();
+        return s.length();
     }
 }
