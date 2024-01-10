4ecc088796543d43d829f92e6f718696977ef40d
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-01-10 20:56:18 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (35.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,9 @@
 class Solution {
     int[] qb= new int[46];
     public int climbStairs(int n) {
-        if(n==2||n==1){
-            return n;
-        }
-        if(qb[n]!=0){
-            return qb[n];
-        }
-        
-        int stair=climbStairs(n-1)+climbStairs(n-2);
-        qb[n]=stair;
-            return stair;
+        if(n==2||n==1) return n;
+        if(qb[n]!=0) return qb[n];
+        qb[n]=climbStairs(n-1)+climbStairs(n-2);
+        return qb[n];
     }
 }
