84dc115b9852f94ceb6a32446105581ff83448a6
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-01-16 20:35:47 +0530

    Time: 19 ms (6.07%), Space: 45.1 MB (8.64%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,13 +1,12 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        
         ArrayList<Integer> p=new ArrayList<>();
         int a=0;
         for(int i =0; i<prices.length-1; i++){
             for(int j=i+1; j<prices.length; j++){
                 if(money >=0){
                 p.add(prices[i]+prices[j]);
-            }
+                }
             }
         }
         Collections.sort(p);
