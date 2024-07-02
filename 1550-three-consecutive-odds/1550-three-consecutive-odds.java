dcd665024c0cdac1049a05ba5e6620b653bcb790
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-07-02 15:46:58 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (42.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,15 +1,18 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         for(int i=0; i<arr.length-2; i++){
+            if(arr.length==3){
+                i=0;
+            }
             if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
                 return true;
             }
         }
-        if(arr.length==3){
-            if(arr[0]%2!=0 && arr[1]%2!=0 && arr[2]%2!=0){
-                return true;
-            }
-        }
+        // if(arr.length==3){
+        //     if(arr[0]%2!=0 && arr[1]%2!=0 && arr[2]%2!=0){
+        //         return true;
+        //     }
+        // }
         return false;
     }
 }
