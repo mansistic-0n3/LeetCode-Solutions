ae73d46034e71219bcf6814ff55d4f36e0d925c8
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-07-04 14:33:04 +0530

    Time: 10 ms (16.45%), Space: 45.2 MB (63.12%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.java
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// class Solution {
+//     public int numComponents(ListNode head, int[] nums) {
+//         Arrays.sort(nums);
+//         int count = 0;
+//         boolean inComponent = false;
+
+//         ListNode current = head;
+//         int i = 0;
+//         int len = nums.length;
+
+//         while (current != null) {
+//             while (i < len && nums[i] < current.val) {
+//                 i++;
+//             }
+            
+//             if (i < len && nums[i] == current.val) {
+//                 if (!inComponent) {
+//                     count++;
+//                     inComponent = true;
+//                 }
+//             } else {
+//                 inComponent = false;
+//             }
+
+//             current = current.next;
+//         }
+
+//         return count;
+//     }
+// }
+
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        // Sort the nums array
+        Arrays.sort(nums);
+
+        int count = 0;
+        boolean inComponent = false;
+
+        // Traverse the linked list
+        ListNode current = head;
+        while (current != null) {
+            if (binarySearch(nums, current.val)) {
+                if (!inComponent) {
+                    // Start of a new component
+                    count++;
+                    inComponent = true;
+                }
+            } else {
+                // End of a component
+                inComponent = false;
+            }
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    // Helper method for binary search
+    private boolean binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
