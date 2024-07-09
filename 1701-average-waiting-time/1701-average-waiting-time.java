fddfa93233c936b04ffc9f179e0828db101d5f47
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-07-09 23:02:06 +0530

    Time: 4 ms (42.86%), Space: 73 MB (24.49%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -5,17 +5,13 @@ class Solution {
         double sum = 0;
         double avg = 0.0;
         for(int i=0; i<customers.length; i++){
-            if(i==0){
-                count = customers[0][0]+customers[0][1];
+            if(count>customers[i][0]){
+                count=count+customers[i][1];
             }else{
-                if(count>customers[i][0]){
-                    count=count+customers[i][1];
-                }else{
-                    count = customers[i][0]+customers[i][1];
-                }
+                count = customers[i][0]+customers[i][1];
             }
-            total = count - customers[i][0];
-            sum=sum+total;
+        total = count - customers[i][0];
+        sum=sum+total;
         }
         avg=sum/customers.length;
         return avg;
