cfcdc2b06c89f2744caf6b86719026197ac5dec3
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-08-06 17:37:29 +0530

    Time: 2133 ms (5.24%), Space: 45.1 MB (72.75%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,13 +1,34 @@
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuilder sb = new StringBuilder();
+        List<Character> ch = new ArrayList<>();
         for (char c : s.toCharArray()) {
-            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
-                sb.deleteCharAt(sb.length() - 1);
-            } else {
-                sb.append(c);
+            ch.add(c);
+        }
+        for(int i=0; i<ch.size()-1; i++){
+            if(ch.get(i)==ch.get(i+1)){
+                ch.remove(i);
+                ch.remove(i);
+                i=-1;
             }
+        } StringBuilder sb = new StringBuilder();
+        for (Character c : ch) {
+            sb.append(c);
         }
         return sb.toString();
     }
 }
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         for (char c : s.toCharArray()) {
+//             if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
+//                 sb.deleteCharAt(sb.length() - 1);
+//             } else {
+//                 sb.append(c);
+//             }
+//         }
+        
+//         return sb.toString();
+//     }
+// }
