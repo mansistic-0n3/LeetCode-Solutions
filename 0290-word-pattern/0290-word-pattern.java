513dfa4b60f1ad35c7c7019590c84f682496239a
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-08-10 20:52:14 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (16.95%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,36 +1,24 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        // Map to store the relationship between pattern characters and words
-        HashMap<Character, String> map = new HashMap<>();
-        
-        // Set to track words that have already been mapped
+        HashMap<Character, String> map= new HashMap();
         HashSet<String> set = new HashSet<>();
-        
-        // Split the input string into an array of words
         String[] input = s.split(" ");
-        
-        // Check if the number of words matches the length of the pattern
-        if (input.length != pattern.length()) {
+        if(input.length!=pattern.length()){
             return false;
         }
-        
-        for (int i = 0; i < pattern.length(); i++) {
-            if (map.containsKey(pattern.charAt(i))) {
-                // Check if the current word matches the previously mapped word for this character
-                if (!input[i].equals(map.get(pattern.charAt(i)))) {
+        for(int i=0; i<pattern.length(); i++){
+            if(map.containsKey(pattern.charAt(i))){
+                if(!input[i].equals(map.get(pattern.charAt(i)))){
                     return false;
                 }
-            } else {
-                // If the word is already mapped to another character, return false
+            }else{
                 if (set.contains(input[i])) {
                     return false;
                 }
-                // Map the character to the current word and add the word to the set
                 set.add(input[i]);
                 map.put(pattern.charAt(i), input[i]);
             }
         }
-        
         return true;
     }
 }
