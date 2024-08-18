e73444fed3d0334d42f839844145744c7e25d5c7
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-08-18 19:46:59 +0530

    Time: 873 ms (80.97%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -4,4 +4,4 @@ Round((Count(r.contest_id)*100/u.total_users),2) as percentage
 from register r join
 (SELECT COUNT(DISTINCT user_id) AS total_users FROM users) u
 # on users.user_id=register.user_id 
-group by r.contest_id order by percentage desc, contest_id asc;
+group by contest_id order by percentage desc, contest_id asc;
