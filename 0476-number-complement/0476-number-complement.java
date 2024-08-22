5133224641dd9cd75460d76af863fb1ea7c84db8
Mansi <71551475+mansistic-0n3@users.noreply.github.com>
2024-08-22 19:23:38 +0530

    Time: 2 ms (12.59%), Space: 40.2 MB (67.03%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findComplement(int num) {
+        String bi = Integer.toBinaryString(num);
+        String res="";
+        for(int i=0; i<bi.length(); i++){
+            if(bi.charAt(i) == '0'){
+                res += '1';
+            }
+            else{
+                res += '0';
+            }
+        }
+        int decimal = Integer.parseInt(res, 2);
+        return decimal;
+    }
+}
